45aeddbf07d380765e9b54a8b67fceb1c5887fd4
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2025-07-09 18:16:36 +0200

    firebase init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,4 +37,15 @@ export default defineNuxtConfig({
     defaultLocale: "pl",
     vueI18n: "./i18n.config.ts",
   },
+
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    },
+  },
 });
