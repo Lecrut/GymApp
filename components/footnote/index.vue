bde9b243dd6c532128b8878fef0f784cfcac0da6
Blindeen <kuba.szmajda@gmail.com>
2025-07-10 15:54:59 +0200

    style footer

--- components/footnote/index.vue
+++ components/footnote/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <v-footer>GymApp</v-footer>
+  <v-footer>
+    <v-container class="pt-5 pb-5" min-width="100%">
+      <v-row justify="center" align="center">
+        <v-col cols="auto">
+          &#169; {{ new Date().getFullYear() }} <strong>GymApp</strong>. All
+          rights reserved.
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
 </template>
